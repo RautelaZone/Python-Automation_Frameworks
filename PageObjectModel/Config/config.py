0c3621770b8b7ed8d1972b949b86802ffe52642f
RautelaZone <anilrautela051090@gmail.com>
2021-03-24 23:03:36 +0530

    Added files

---
+++ PageObjectModel/Config/config.py
@@ -0,0 +1,8 @@
+class TestData:
+    CHROME_EXECUTABLE_PATH = "/PageObjectModel/Drivers/chromedriver"
+    FIREFOX_EXECUTABLE_PATH = "/PageObjectModel/Drivers/geckodriver"
+    BASE_URL = "https://app.hubspot.com/login"
+    USER_NAME = "naveenanimation30@gmail.com"
+    PASSWORD = "Selenium@12345"
+
+    LOGIN_PAGE_TITLE = "HubSpot Login"
