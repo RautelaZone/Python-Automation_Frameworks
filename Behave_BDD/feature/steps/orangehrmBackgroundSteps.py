87a0226a5dc6ee767c967d87242fc2c87606a685
RautelaZone <anilrautela051090@gmail.com>
2021-04-22 23:52:49 +0530

    [Added Behave BDD Framework folder]

---
+++ Behave_BDD/feature/steps/orangehrmBackgroundSteps.py
@@ -0,0 +1,43 @@
+"""
+Background is a gerhkin keyword which is used to store those steps which are common to
+each Scenario and needs to be executed before.
+"""
+
+from behave import *
+
+@given('I launch browser')
+def step_impl(context):
+    assert True,"Launching Browser"
+
+@when('I open application')
+def step_impl(context):
+    assert True, "Open application"
+
+@when('Enter valid username and password')
+def step_impl(context):
+    assert True,"Entered valid credentials"
+
+@then('click on Login')
+def step_impl(context):
+    assert True,"Clicked to Login button"
+
+@then('user must login to Dashboard Page')
+def step_impl(context):
+    assert True,"Landed to Dashboard page"
+
+@when('navigate to search page')
+def step_impl(context):
+    assert True,"Navigated to Search Page"
+
+@then('search page should display')
+def step_impl(context):
+    assert True,"Search page is displaying"
+
+@when('navigate to advance search page')
+def step_impl(context):
+    assert True,"Navigated to Advance Search Page"
+
+@then('advance search page should display')
+def step_impl(context):
+    assert True,"Advance Search page is displaying"
+
